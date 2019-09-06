7ac8d5e50a93e7ad0ce6a3453b05bf85811b649d
NathanielDaniels <26263599+NathanielDaniels@users.noreply.github.com>
2019-09-05 21:13:23 -0500

    working on Javascript

---
+++ Javascript/script.js
@@ -0,0 +1,11 @@
+switch (name) {
+  case name === "Nate":
+    console.log("Name is Nate");
+    break;
+  case name === "Susan":
+    console.log("name is Susan");
+    break;
+  default:
+    console.log("did you enter name?");
+    break;
+}
