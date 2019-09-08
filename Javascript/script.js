c8a425e7db3700eb0148b6319f5468e435a339fa
NathanielDaniels <26263599+NathanielDaniels@users.noreply.github.com>
2019-09-08 15:43:14 -0500

    added exercise 7 files

--- Javascript/script.js
+++ Javascript/script.js
@@ -33,33 +33,81 @@
 
 //* Exercise 6
 
-let array = ["Banana", "Apples", "Oranges", "Blueberries"];
+// let array = ["Banana", "Apples", "Oranges", "Blueberries"];
 
-let banana = array.shift();
-console.log(banana);
+// let banana = array.shift();
+// console.log(banana);
 
-array.sort();
-console.log(array);
+// array.sort();
+// console.log(array);
 
-array.push("kiwi");
-console.log(array);
+// array.push("kiwi");
+// console.log(array);
 
-let apples = array.splice(0, 1);
-console.log(apples);
+// let apples = array.splice(0, 1);
+// console.log(apples);
 
-console.log(array);
+// console.log(array);
 
-array.reverse();
+// array.reverse();
 
-console.log(array);
+// console.log(array);
 
-var array2 = ["Banana", ["Apples", ["Oranges"], "Blueberries"]];
+// var array2 = ["Banana", ["Apples", ["Oranges"], "Blueberries"]];
 
-let oranges = array2[1][1][0];
-console.log(oranges);
+// let oranges = array2[1][1][0];
+// console.log(oranges);
 
 //?======================================
+//! Objects
 
+let user = {
+  //? Property: Value,
+  name: "Nathan",
+  age: 33,
+  hobby: "coding",
+  isMarried: false,
+  shout: () => {
+    console.log("Loud Noises!");
+  }
+};
+
+console.log(user);
+console.log(user.name);
+
+user.lastName = "Daniels";
+console.log(user);
+
+//* Run the arrow function inside user object
+//? this is called a Method
+user.shout();
+
+//* Change user name
+// user.name = "stan";
+// console.log(user);
+
+//* if statement from Object Property Value
+if (user.name === "Nathan" || user.lastName === "Daneils") {
+  console.log(`Welcome in, ${user.name} ${user.lastName}!`);
+} else {
+  console.log(`You're Not Welcome Here`);
+}
+
+//* Object inside Array
+
+let list = [
+  {
+    username: "Andy",
+    password: "secret"
+  },
+  {
+    username: "Stan",
+    password: "123"
+  }
+];
+
+//* pull username from 2nd object inside list array
+console.log(list[1].username);
 //?======================================
 
 //?======================================
