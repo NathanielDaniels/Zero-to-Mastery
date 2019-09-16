8f0765f62533f6dae877593638360bbe8bcf7fb6
NathanielDaniels <26263599+NathanielDaniels@users.noreply.github.com>
2019-09-16 09:57:57 -0500

    working on random color when click button

--- background-gen/script.js
+++ background-gen/script.js
@@ -2,12 +2,18 @@ var css = document.querySelector("h3");
 var color1 = document.querySelector(".color1");
 var color2 = document.querySelector(".color2");
 var body = document.getElementById("gradient");
+let randomButton = document.getElementsByClassName("randomButton")[0];
 
 function setGradient() {
   body.style.background = `linear-gradient(60deg, ${color1.value},${color2.value})  `;
   css.textContent = `${body.style.background};`;
 }
 
-color1.addEventListener("input", setGradient);
+setGradient();
 
+color1.addEventListener("input", setGradient);
 color2.addEventListener("input", setGradient);
+
+randomButton.addEventListener("click", () => {
+  setGradient(random()); //! FIGURE THIS OUT
+});
