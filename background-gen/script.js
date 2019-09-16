7e88f54e3edd9f7c34bc0cbdc71b30c4c32227d3
NathanielDaniels <26263599+NathanielDaniels@users.noreply.github.com>
2019-09-16 02:25:00 -0500

    working on Background Generator

--- background-gen/script.js
+++ background-gen/script.js
@@ -4,14 +4,8 @@ var color2 = document.querySelector(".color2");
 var body = document.getElementById("gradient");
 
 function setGradient() {
-	body.style.background = 
-	"linear-gradient(to right, " 
-	+ color1.value 
-	+ ", " 
-	+ color2.value 
-	+ ")";
-
-	css.textContent = body.style.background + ";";
+  body.style.background = `linear-gradient(60deg, ${color1.value},${color2.value})  `;
+  css.textContent = `${body.style.background};`;
 }
 
 color1.addEventListener("input", setGradient);
