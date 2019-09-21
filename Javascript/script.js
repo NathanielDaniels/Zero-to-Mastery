49c6e02d923f2eaafcff6d7e8a356b8384bb79ce
NathanielDaniels <26263599+NathanielDaniels@users.noreply.github.com>
2019-09-20 23:23:47 -0500

    moving to objects

--- Javascript/script.js
+++ Javascript/script.js
@@ -516,19 +516,19 @@
 //* MAP/ FILTER/ REDUCE
 
 //* Map
-// const mapArray = array.map(num => num * 2);
+const mapArray = array.map(num => num * 2);
 //? Map -> Always Must use Return (unless shorthand like above)
-// console.log("Map", mapArray);
+console.log("Map", mapArray);
 
 //* Filter
-// const filterArray = array.filter(num => num < 5);
+const filterArray = array.filter(num => num < 5);
 //? Filter -> Just like Map, use Return (unless shorthand like above)
-// console.log("filter", filterArray);
+console.log("filter", filterArray);
 
 //* Reduce
-// const reduceArray = array.reduce((accumulator, num) => {
-// return accumulator + num;
-// }, 0);
+const reduceArray = array.reduce((accumulator, num) => {
+  return accumulator + num;
+}, 0);
 //? Accumulator is something that stores info thats happens in the body. It adds one to the next, so 1 + 2 = 3, 3 + 10 = 13, 13 + 16 = 29. So it reduces the array down to one number
 
 // console.log("reduce", reduceArray);
