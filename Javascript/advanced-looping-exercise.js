98ef1e1c5d522b1d661cf5335bcf0ee99045a769
NathanielDaniels <26263599+NathanielDaniels@users.noreply.github.com>
2019-09-24 10:09:41 -0500

    completed Advanced Looping Exercise

--- Javascript/advanced-looping-exercise.js
+++ Javascript/advanced-looping-exercise.js
@@ -6,22 +6,24 @@ const detailedBasket = {
 };
 
 //1
-for (let i = 0; i < basket.length; i++) {
-  console.log(basket[i]);
-}
+// for (let i = 0; i < basket.length; i++) {
+//   console.log(basket[i]);
+// }
 
 //2
-basket.forEach(item => {
-  console.log(item);
-});
+// basket.forEach(item => {
+//   console.log(item);
+// });
 
-for (item in detailedBasket) {
-  console.log(item);
-}
+// for (item in detailedBasket) {
+//   console.log(item);
+// }
 
-for (item of basket) {
-  console.log(item);
-}
+// for (item of basket) {
+//   console.log(item);
+// }
+
+console.log("============================");
 
 // Question #1:
 // create a function called biggestNumberInArray() that takes
@@ -32,28 +34,36 @@ const array = [-1, 0, 3, 100, 99, 2, 99]; // should return 100
 const array2 = ["a", 3, 4, 2]; // should return 4
 const array3 = []; // should return 0
 
-// function biggestNumberInArray(arr) {
-//   let highest = 0;
-//   for (let i = 0; i < arr.length; i++) {
-//     if (highest < arr[i]) {
-//       highest = arr[i];
-//     }
-//   }
-//   return highest;
-// }
-
-function biggestNumberInArray2(arr) {
+function biggestNumberInArray(arr) {
   let highest = 0;
-  for (i of arr) {
-    // if (highest < arr[i]) {
-    //   highest = arr[i];
-    // }
-    console.log(i);
+  for (let i = 0; i < arr.length; i++) {
+    if (highest < arr[i]) {
+      highest = arr[i];
+    }
   }
-  // return highest;
+  return highest;
 }
 
-function biggestNumberInArray3(arr) {}
+function biggestNumberInArray2(arr) {
+  let highest2 = 0;
+  for (i of arr) {
+    if (highest2 < i) {
+      highest2 = i;
+    }
+    // console.log(i);
+  }
+  return highest2;
+}
+
+function biggestNumberInArray3(arr) {
+  let highest3 = 0;
+  arr.forEach(i => {
+    if (highest3 < i) {
+      highest3 = i;
+    }
+  });
+  return highest3;
+}
 
 // Question #2:
 // Write a function checkBasket() that lets you know if the item is in the basket or not
@@ -63,4 +73,31 @@ amazonBasket = {
   floss: 100
 };
 
-function checkBasket(basket, lookingFor) {}
+//======================================================
+//! The way i did it was to change the keys in object into an array, then iterate through the array. It Technically works, but not the best way.
+const basketArray = Object.keys(amazonBasket); //turns object keys into an array (["glasses", "books", "floss"])
+
+// console.log(basketArray);
+
+// function checkBasket(basket, lookingFor) {
+//   if (basket.includes(lookingFor) === true) {
+//     console.log("amazonBasket Includes: ", lookingFor);
+//   } else {
+//     console.log("amazonBasket Does NOT Include: ", lookingFor);
+//   }
+// }
+
+// checkBasket(basketArray, "floss");
+
+//======================================================
+//! The way it should be done w/ for in loop. Much cleaner and way less code
+function checkBasket(basket, lookingFor) {
+  for (item in basket) {
+    if (item === lookingFor) {
+      return `${item} currently in the basket`;
+    }
+  }
+  return `${item} currently NOT in the basket`;
+}
+
+checkBasket(amazonBasket, "glasses");
