562300d08cf4d3b05f84a4931853d7ec27db1aa8
NathanielDaniels <26263599+NathanielDaniels@users.noreply.github.com>
2019-09-25 12:04:33 -0500

    onto How Javasript Works

--- Javascript/script.js
+++ Javascript/script.js
@@ -813,10 +813,11 @@
 //?==========================================
 //! Debugging!
 
-const flattened = [[0, 1], [2, 3], [4, 5]].reduce((accumulator, array) => {
-  console.log("array", array);
-  console.log("accumulator", accumulator);
-  return accumulator.concat(array);
-}, []);
+// const flattened = [[0, 1], [2, 3], [4, 5]].reduce((accumulator, array) => {
+//   console.log("array", array);
+//   console.log("accumulator", accumulator);
+//   return accumulator.concat(array);
+// }, []);
 
-// console.log(flattened);
+//?==========================================
+//! How Javascript Works
