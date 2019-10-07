3f5b249f908a1c794d6ca5920d94fa5db6064368
NathanielDaniels <26263599+NathanielDaniels@users.noreply.github.com>
2019-10-06 23:31:40 -0500

    starting Promise exercise

--- Javascript/script.js
+++ Javascript/script.js
@@ -931,20 +931,20 @@
 
 //===============
 
-const urls = [
-  "https://jsonplaceholder.typicode.com/users",
-  "https://jsonplaceholder.typicode.com/posts",
-  "https://jsonplaceholder.typicode.com/albums"
-];
+// const urls = [
+//   "https://jsonplaceholder.typicode.com/users",
+//   "https://jsonplaceholder.typicode.com/posts",
+//   "https://jsonplaceholder.typicode.com/albums"
+// ];
 
-Promise.all(
-  urls.map(url => {
-    return fetch(url).then(resp => resp.json());
-  })
-)
-  .then(results => {
-    console.log(results[0]);
-    console.log(results[2]);
-    console.log(results[3]);
-  })
-  .catch(console.log("error"));
+// Promise.all(
+//   urls.map(url => {
+//     return fetch(url).then(resp => resp.json());
+//   })
+// )
+//   .then(results => {
+//     console.log(results[0]);
+//     console.log(results[2]);
+//     console.log(results[3]);
+//   })
+//   .catch(console.log("error"));
